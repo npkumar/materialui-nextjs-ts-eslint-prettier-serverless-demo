2a3e4f72cb070abfb20fdadabe62e6fd9c33307a
Nitin Pradeep Kumar <nitinpradeep.kumar@oyolife.co.jp>
2021-02-25 15:58:38 +0900

    Non pem drawer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head';
 import { AppProps } from 'next/app';
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import clsx from 'clsx';
 import theme from '../src/theme';
 
 import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
@@ -15,11 +16,81 @@ import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import { Container, Grid } from '@material-ui/core';
-import HomeIcon from '@material-ui/icons/Home';
-import EqualizerIcon from '@material-ui/icons/Equalizer';
-import TimelineIcon from '@material-ui/icons/Timeline';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import { Badge, Box, Container, IconButton } from '@material-ui/core';
 import Link from 'next/link';
+
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import BarChartIcon from '@material-ui/icons/BarChart';
+import LayersIcon from '@material-ui/icons/Layers';
+import AssignmentIcon from '@material-ui/icons/Assignment';
+
+function Copyright() {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link href="/">Kakaku</Link> {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
+export const mainListItems = (
+    <div>
+        <Link href="/">
+            <ListItem button>
+                <ListItemIcon>
+                    <DashboardIcon />
+                </ListItemIcon>
+                <ListItemText primary="Dashboard" />
+            </ListItem>
+        </Link>
+        <Link href="/stats">
+            <ListItem button>
+                <ListItemIcon>
+                    <BarChartIcon />
+                </ListItemIcon>
+                <ListItemText primary="Stats" />
+            </ListItem>
+        </Link>
+        <Link href="/revenue">
+            <ListItem button>
+                <ListItemIcon>
+                    <LayersIcon />
+                </ListItemIcon>
+                <ListItemText primary="Revenue" />
+            </ListItem>
+        </Link>
+    </div>
+);
+
+export const secondaryListItems = (
+    <div>
+        <ListSubheader inset>Forecast reports</ListSubheader>
+        <ListItem button>
+            <ListItemIcon>
+                <AssignmentIcon />
+            </ListItemIcon>
+            <ListItemText primary="Next month" />
+        </ListItem>
+        <ListItem button>
+            <ListItemIcon>
+                <AssignmentIcon />
+            </ListItemIcon>
+            <ListItemText primary="Next quarter" />
+        </ListItem>
+        <ListItem button>
+            <ListItemIcon>
+                <AssignmentIcon />
+            </ListItemIcon>
+            <ListItemText primary="Next year" />
+        </ListItem>
+    </div>
+);
 
 const drawerWidth = 240;
 
@@ -28,31 +99,94 @@ const useStyles = makeStyles((theme: Theme) =>
         root: {
             display: 'flex',
         },
-        appBar: {
-            width: `calc(100% - ${drawerWidth}px)`,
-            marginLeft: drawerWidth,
+        toolbar: {
+            paddingRight: 24, // keep right padding when drawer closed
         },
-        drawer: {
-            width: drawerWidth,
-            flexShrink: 0,
+        toolbarIcon: {
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'flex-end',
+            padding: '0 8px',
+            ...theme.mixins.toolbar,
+        },
+        appBar: {
+            zIndex: theme.zIndex.drawer + 1,
+            transition: theme.transitions.create(['width', 'margin'], {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.leavingScreen,
+            }),
+        },
+        appBarShift: {
+            marginLeft: drawerWidth,
+            width: `calc(100% - ${drawerWidth}px)`,
+            transition: theme.transitions.create(['width', 'margin'], {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.enteringScreen,
+            }),
+        },
+        menuButton: {
+            marginRight: 36,
+        },
+        menuButtonHidden: {
+            display: 'none',
+        },
+        title: {
+            flexGrow: 1,
+            textTransform: 'uppercase',
         },
         drawerPaper: {
+            position: 'relative',
+            whiteSpace: 'nowrap',
             width: drawerWidth,
+            transition: theme.transitions.create('width', {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.enteringScreen,
+            }),
         },
-        // necessary for content to be below app bar
-        toolbar: theme.mixins.toolbar,
+        drawerPaperClose: {
+            overflowX: 'hidden',
+            transition: theme.transitions.create('width', {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.leavingScreen,
+            }),
+            width: theme.spacing(7),
+            [theme.breakpoints.up('sm')]: {
+                width: theme.spacing(9),
+            },
+        },
+        appBarSpacer: theme.mixins.toolbar,
         content: {
             flexGrow: 1,
-            backgroundColor: theme.palette.background.default,
-            padding: theme.spacing(3),
-            marginLeft: drawerWidth,
+            height: '100vh',
+            overflow: 'auto',
+        },
+        container: {
+            paddingTop: theme.spacing(4),
+            paddingBottom: theme.spacing(4),
+        },
+        paper: {
+            padding: theme.spacing(2),
+            display: 'flex',
+            overflow: 'auto',
+            flexDirection: 'column',
+        },
+        fixedHeight: {
+            height: 240,
         },
     }),
 );
-
 export default function MyApp(props: AppProps) {
     const { Component, pageProps } = props;
+
     const classes = useStyles();
+    const [open, setOpen] = React.useState(true);
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+    const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
 
     React.useEffect(() => {
         // Remove the server-side injected CSS.
@@ -63,74 +197,65 @@ export default function MyApp(props: AppProps) {
     }, []);
 
     return (
-        <React.Fragment>
+        <>
             <Head>
                 <title>My page</title>
                 <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
             </Head>
             <ThemeProvider theme={theme}>
-                {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-                <CssBaseline />
-                <AppBar position="fixed" className={classes.appBar}>
-                    <Toolbar>
-                        <Typography variant="h6" noWrap>
-                            Permanent drawer
-                        </Typography>
-                    </Toolbar>
-                </AppBar>
-                <Drawer
-                    className={classes.drawer}
-                    variant="permanent"
-                    classes={{
-                        paper: classes.drawerPaper,
-                    }}
-                    anchor="left"
-                >
-                    <div className={classes.toolbar}>
-                        <Grid container alignContent="center" className={classes.toolbar}>
-                            <Grid item xs={12}>
-                                <Typography variant="h5" align="center" component="div">
-                                    KAKAKU
-                                </Typography>
-                            </Grid>
-                        </Grid>
-                    </div>
-                    <Divider />
-                    <List>
-                        <Link href="/">
-                            <ListItem button key="Home">
-                                <ListItemIcon>
-                                    <HomeIcon />
-                                </ListItemIcon>
-                                <ListItemText primary="Home" />
-                            </ListItem>
-                        </Link>
-                        <Link href="/stats">
-                            <ListItem button key="Stats">
-                                <ListItemIcon>
-                                    <EqualizerIcon />
-                                </ListItemIcon>
-                                <ListItemText primary="Stats" />
-                            </ListItem>
-                        </Link>
-                        <Link href="/revenue">
-                            <ListItem button key="Future Revenue">
-                                <ListItemIcon>
-                                    <TimelineIcon />
-                                </ListItemIcon>
-                                <ListItemText primary="Future Revenue" />
-                            </ListItem>
-                        </Link>
-                    </List>
-                </Drawer>
+                <div className={classes.root}>
+                    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+                    <CssBaseline />
+                    <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>
+                        <Toolbar className={classes.toolbar}>
+                            <IconButton
+                                edge="start"
+                                color="inherit"
+                                aria-label="open drawer"
+                                onClick={handleDrawerOpen}
+                                className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
+                            >
+                                <MenuIcon />
+                            </IconButton>
+                            <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
+                                Kakaku
+                            </Typography>
+                            <IconButton color="inherit">
+                                <Badge badgeContent={4} color="secondary">
+                                    <NotificationsIcon />
+                                </Badge>
+                            </IconButton>
+                        </Toolbar>
+                    </AppBar>
+                    <Drawer
+                        variant="permanent"
+                        classes={{
+                            paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
+                        }}
+                        open={open}
+                    >
+                        <div className={classes.toolbarIcon}>
+                            <IconButton onClick={handleDrawerClose}>
+                                <ChevronLeftIcon />
+                            </IconButton>
+                        </div>
+                        <Divider />
+                        <List>{mainListItems}</List>
+                        <Divider />
+                        <List>{secondaryListItems}</List>
+                    </Drawer>
 
-                <main className={classes.content}>
-                    <div className={classes.toolbar} />
-                    <Container>
-                        <Component {...pageProps} />
-                    </Container>
-                </main>
+                    <main className={classes.content}>
+                        <div className={classes.appBarSpacer} />
+                        <Container maxWidth="lg" className={classes.container}>
+                            <Component {...pageProps} />
+                            <Box pt={4}>
+                                <Copyright />
+                            </Box>
+                        </Container>
+                    </main>
+                </div>
             </ThemeProvider>
-        </React.Fragment>
+        </>
     );
 }
